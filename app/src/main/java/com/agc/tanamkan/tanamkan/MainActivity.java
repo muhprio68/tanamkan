01481f8078513521d1858c8e18413f8bd48d7c04
muhprio68 <muhammadprio93@gmail.com>
2018-11-22 18:50:21 +0700

    fisrt commit

---
+++ app/src/main/java/com/agc/tanamkan/tanamkan/MainActivity.java
@@ -0,0 +1,13 @@
+package com.agc.tanamkan.tanamkan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+    }
+}
