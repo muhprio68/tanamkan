74b49fa44b4c97380ad3a83236039d25650569c6
muhprio68 <muhammadprio93@gmail.com>
2018-11-23 21:33:57 +0700

    penambahan app bar

--- app/src/main/java/com/agc/tanamkan/tanamkan/HomeFragment.java
+++ app/src/main/java/com/agc/tanamkan/tanamkan/HomeFragment.java
@@ -2,7 +2,6 @@ package com.agc.tanamkan.tanamkan;
 
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,12 +10,14 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+
 
 
     public HomeFragment() {
@@ -28,6 +29,20 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.homeToolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayShowTitleEnabled(false);
+        TextView mTitle = (TextView) toolbar.findViewById(R.id.tbTitle);
         return view;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);//Make sure you have this line of code.
+    }
+
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.homebar_menu, menu);
     }
 }
