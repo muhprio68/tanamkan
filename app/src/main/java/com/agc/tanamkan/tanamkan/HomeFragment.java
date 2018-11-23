c3e347f562ed4adc7a8c14b1d41adf893c4d7920
muhprio68 <muhammadprio93@gmail.com>
2018-11-23 10:40:20 +0700

    tambah layouting

---
+++ app/src/main/java/com/agc/tanamkan/tanamkan/HomeFragment.java
@@ -0,0 +1,33 @@
+package com.agc.tanamkan.tanamkan;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
+}
--- app/src/main/java/com/agc/tanamkan/tanamkan/MainActivity.java
+++ app/src/main/java/com/agc/tanamkan/tanamkan/MainActivity.java
@@ -1,13 +1,54 @@
 package com.agc.tanamkan.tanamkan;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen);
+        loadFragment(new HomeFragment());
+// inisialisasi BottomNavigaionView
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bn_main);
+// beri listener pada saat item/menu bottomnavigation terpilih
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fl_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()) {
+            case R.id.home_menu:
+                fragment = new HomeFragment();
+                break;
+            case R.id.tanaman_menu:
+                fragment = new TanamanFragment();
+                break;
+            case R.id.pot_menu:
+                fragment = new PotFragment();
+                break;
+            case R.id.profil_menu:
+                fragment = new ProfilFragment();
+                break;
+        }
+        return loadFragment(fragment);
     }
 }
